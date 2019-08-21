e921d3553e0acd688533187cc76a9ad17f59c7c9
billyliberty <joshua.crum@gmail.com>
2019-08-21 12:27:35 -0500

    Added actions.

--- app/controllers/supply_items_controller.rb
+++ app/controllers/supply_items_controller.rb
@@ -1,2 +1,16 @@
 class SupplyItemsController < ApplicationController
+
+    def index
+        @supply_list = SupplyList.new
+        @supply_lists = SupplyList.all 
+        render 'supply_lists/index.html.erb'
+    end
+
+    def show
+        @supply_list = SupplyList.find(params[:id])
+    end
+
+    def create
+    end
+
 end
