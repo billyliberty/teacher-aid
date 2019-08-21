046b329c379f2ab0df45501402b168b93327b0d4
billyliberty <joshua.crum@gmail.com>
2019-08-21 15:21:58 -0500

    Added database information.

--- db/migrate/20190821151131_create_supply_items.rb
+++ db/migrate/20190821151131_create_supply_items.rb
@@ -1,6 +1,9 @@
 class CreateSupplyItems < ActiveRecord::Migration[5.2]
   def change
     create_table :supply_items do |t|
+      t.string :item 
+      t.integer :value 
+      t.references :supply_list, foreign_key :true
 
       t.timestamps
     end
