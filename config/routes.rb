765c4ce1cf1ffe5875d01b57da37df20412e3432
billyliberty <joshua.crum@gmail.com>
2019-08-21 18:05:54 -0500

    Removed unnecessary routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :students
-  resources :supply_items
-  resources :supply_lists
   resources :parents
   resources :teachers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
