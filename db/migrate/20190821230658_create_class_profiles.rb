f80f2704172ade20f25f4c51882cfcf89b7e459e
billyliberty <joshua.crum@gmail.com>
2019-08-21 18:11:41 -0500

    Updated class_profile database migration.

--- db/migrate/20190821230658_create_class_profiles.rb
+++ db/migrate/20190821230658_create_class_profiles.rb
@@ -1,6 +1,13 @@
 class CreateClassProfiles < ActiveRecord::Migration[5.2]
   def change
     create_table :class_profiles do |t|
+      t.belongs_to :teacher, foreign_key: true
+      t.belongs_to :student, foreign_key: true
+      t.string :subject
+      t.integer :grade
+      t.string :items
+      t.text :note
+      t.date :next_test
 
       t.timestamps
     end
