01c883bbadb7d79dd5ff5e66b047017817ad039c
billyliberty <joshua.crum@gmail.com>
2019-08-21 18:13:28 -0500

    Updated database migration for students.

--- db/migrate/20190821230100_create_students.rb
+++ db/migrate/20190821230100_create_students.rb
@@ -1,6 +1,10 @@
 class CreateStudents < ActiveRecord::Migration[5.2]
   def change
     create_table :students do |t|
+      t.string :name
+      t.date :dob
+      t.integer :gender
+      t.references :parent, foreign_key: true
 
       t.timestamps
     end
