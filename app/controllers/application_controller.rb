a359e92d7eaa9d4f209477fde3fdaefeaa6b6e06
billyliberty <joshua.crum@gmail.com>
2019-08-21 12:23:42 -0500

    Updated application controller with helper methods for current user and if they're logged in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_teacher, :logged_in_teacher?
+
+    private
+
+        def current_teacher
+            @current_teacher ||= Teacher.find_by_id(session[:teacher_id]) if se
+        end
+
+        def logged_in_teacher?
+            !!session[:teacher_id]
+        end
+
+        def redirect_if_not_logged_in
+            redirect to '/' if !logged_in_teacher?
+        end
+
 end
