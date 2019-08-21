fc2ea26f144d24a06f95c669ffd076829c6678a5
billyliberty <joshua.crum@gmail.com>
2019-08-21 10:11:05 -0500

    Generated supply_list resource.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :supply_lists
   resources :parents
   resources :teachers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
